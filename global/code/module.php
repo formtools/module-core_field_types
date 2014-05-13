a5842753bd335a97846ffe0742143ee1fd4fde84
Ben Keen <ben.keen@gmail.com>
2014-05-12 17:12:34 -0700

    1.0.2

--- global/code/module.php
+++ global/code/module.php
@@ -14,6 +14,112 @@ function core_field_types__install($module_id)
 }
 
 
+function core_field_types__update($old_version_info, $new_version_info)
+{
+	global $g_table_prefix;
+
+  $old_version_date = date("Ymd", ft_convert_datetime_to_timestamp($old_version_info["module_date"]));
+
+  // fix for MAJOR issue where the raw field types weren't mapped to Option List settings. This was an issue from Oct - Nov 2011, for all
+  // 2.1.5 - 2.1.8 versions installed during that time.
+  if ($old_version_date < 20111122)
+  {
+  	// dropdowns
+		$select_field_type_id = ft_get_field_type_id_by_identifier("dropdown");
+		$field_info = ft_get_field_type($select_field_type_id, true);
+		$option_list_setting_id = "";
+		foreach ($field_info["settings"] as $setting_info)
+		{
+			if ($setting_info["field_type"] == "option_list_or_form_field")
+			{
+				$option_list_setting_id = $setting_info["setting_id"];
+				break;
+			}
+		}
+		if (!empty($option_list_setting_id))
+		{
+			mysql_query("
+			  UPDATE {$g_table_prefix}field_types
+			  SET    raw_field_type_map_multi_select_id = $option_list_setting_id
+			  WHERE  field_type_id = $select_field_type_id AND
+			         raw_field_type_map_multi_select_id IS NULL
+			  LIMIT 1
+			") or die(mysql_error());
+		}
+
+		$multi_select_field_type_id = ft_get_field_type_id_by_identifier("multi_select_dropdown");
+		$field_info = ft_get_field_type($multi_select_field_type_id, true);
+		$option_list_setting_id = "";
+		foreach ($field_info["settings"] as $setting_info)
+		{
+			if ($setting_info["field_type"] == "option_list_or_form_field")
+			{
+				$option_list_setting_id = $setting_info["setting_id"];
+				break;
+			}
+		}
+		if (!empty($option_list_setting_id))
+		{
+			mysql_query("
+			  UPDATE {$g_table_prefix}field_types
+			  SET    raw_field_type_map_multi_select_id = $option_list_setting_id
+			  WHERE  field_type_id = $multi_select_field_type_id AND
+			         raw_field_type_map_multi_select_id IS NULL
+			  LIMIT 1
+			") or die(mysql_error());
+		}
+
+
+		$radios_field_type_id = ft_get_field_type_id_by_identifier("radio_buttons");
+		$field_info = ft_get_field_type($radios_field_type_id, true);
+		$option_list_setting_id = "";
+		foreach ($field_info["settings"] as $setting_info)
+		{
+			if ($setting_info["field_type"] == "option_list_or_form_field")
+			{
+				$option_list_setting_id = $setting_info["setting_id"];
+				break;
+			}
+		}
+		if (!empty($option_list_setting_id))
+		{
+			mysql_query("
+			  UPDATE {$g_table_prefix}field_types
+			  SET    raw_field_type_map_multi_select_id = $option_list_setting_id
+			  WHERE  field_type_id = $radios_field_type_id AND
+			         raw_field_type_map_multi_select_id IS NULL
+			  LIMIT 1
+			") or die(mysql_error());
+		}
+
+
+		$checkboxes_field_type_id = ft_get_field_type_id_by_identifier("checkboxes");
+		$field_info = ft_get_field_type($checkboxes_field_type_id, true);
+		$option_list_setting_id = "";
+		foreach ($field_info["settings"] as $setting_info)
+		{
+			if ($setting_info["field_type"] == "option_list_or_form_field")
+			{
+				$option_list_setting_id = $setting_info["setting_id"];
+				break;
+			}
+		}
+		if (!empty($option_list_setting_id))
+		{
+			mysql_query("
+			  UPDATE {$g_table_prefix}field_types
+			  SET    raw_field_type_map_multi_select_id = $option_list_setting_id
+			  WHERE  field_type_id = $checkboxes_field_type_id AND
+			         raw_field_type_map_multi_select_id IS NULL
+			  LIMIT 1
+			") or die(mysql_error());
+		}
+  }
+
+  return array(true, "");
+}
+
+
 /**
  * This installation function + module is unique. For Core 2.1.5 and later, it's bundled with all Core scripts
  * and installs the field types. It can't be removed. This function is called explicitly in the main Form
--- module.php
+++ module.php
@@ -4,8 +4,8 @@ $MODULE = array();
 $MODULE["author"]          = "Encore Web Studios";
 $MODULE["author_email"]    = "formtools@encorewebstudios.com";
 $MODULE["author_link"]     = "http://www.encorewebstudios.com";
-$MODULE["version"]         = "1.0.1";
-$MODULE["date"]            = "2011-11-21";
+$MODULE["version"]         = "1.0.2";
+$MODULE["date"]            = "2011-11-22";
 $MODULE["origin_language"] = "en_us";
 
 $MODULE["nav"] = array(
